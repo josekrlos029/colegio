e5c45957635969f8919d8ab0ecca1d49ccc8dadb
Jose Carlos Jimenez Montenegro <josekrlos029@hotmail.com>
2013-07-05 17:03:09 -0500

    Menú Flotante

--- includes/cargaCabecera.php
+++ includes/cargaCabecera.php
@@ -10,6 +10,7 @@
                 <script src="../utiles/js/jquery-1.9.1.min.js" type="text/javascript" ></script>
                 <script src="../utiles/js/easing.js" type="text/javascript" ></script>
                 <script src="../utiles/js/envios.js" type="text/javascript" ></script>
+                <script src="../utiles/js/inicio.js" type="text/javascript" ></script>
                 <script src="../utiles/js/estiloMensaje.js" type="text/javascript"></script>
               <script src="../utiles/js/mootools-core-1.4.5.js" type="text/javascript"></script>
            
