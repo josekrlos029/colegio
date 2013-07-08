b7225bacc8cd346c809308f8be40b9461e4514f8
andy henry <andy henry@andyhenry1>
2013-07-07 23:39:45 -0500

    consulta usuario

--- controlador/Controlador.php
+++ controlador/Controlador.php
@@ -183,7 +183,53 @@ class Controlador {
 $respuesta .= ' 
      
 <div class="contenedorCentro">
-                  
+                 <table aling="right" width="100%"  border="0">
+       <tr>
+           <td align="right" class="color-text-rojo" colspan="3"><h3>Datos Academicos</h3></td>    
+       </tr>
+       <tr>
+           <td class="color-text-rojo">salones</td>
+          <td class="color-text-rojo">Materias</td>
+          <td class="color-text-rojo">Horas</td>
+       </tr>
+        <tr>
+           <td colspan="3"><hr></td>
+       </tr> 
+          '.
+          $resultado=0;
+          $cont=0; .'
+         '. foreach ($cargas as $carg) { .'
+          <tr>
+          <td>'.  $carg->getIdSalon().' </td>
+         '.
+            $materia = new Materia();
+            $materias = $materia->leerMateriaPorId($carg->getIdMateria());
+            foreach ($materias as $mat) { .'
+            <td>'. strtoupper($mat->getNombreMateria()).' </td>
+            <td>
+                '. strtoupper($mat->getHoras())
+                $cont=$mat->getHoras();
+                $resultado=$resultado+$cont;   .' 
+              
+            </td>
+            '. } .'
+            </tr>
+           '. } .' 
+          
+         
+        
+        <tr>
+           <td colspan="3"><hr></td>
+       </tr>   
+       <tr>
+           <td colspan="3" class="color-text-gris" align="center">
+               <h2>Total Horas Semanales:'.$resultado.'</h2>
+           </td>
+       </tr>   
+        <tr>
+           <td colspan="3"><hr></td>
+       </tr> 
+   </table>     
 </div>
 
                   ';
