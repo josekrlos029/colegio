afdcfb70dfa0e2048d8bf7689205018cc289edac
Jose Carlos Jimenez Montenegro <josekrlos029@hotmail.com>
2013-07-10 16:11:25 -0500

    Cabecera Flotante Ok (y) !

--- utiles/js/inicio.js
+++ utiles/js/inicio.js
@@ -1,22 +1,3 @@
-
 $(document).ready(function(){
-                   
-      var altura = $(".cabecera").offset().top; 
-                         
-      $(window).scroll(function(){
-             
-            if($(window).scrollTop() >= altura){
-                         
-                  $(".cabecera").css("margin-top","0");
-                  $(".cabecera").css("position","fixed");
-                               
-            }else{
-                                     
-                  $(".cabecera").css("margin-top","100px");
-                  $(".cabecera").css("position","static");
-                               
-            }
-                         
-      });
-                   
-});
+    $(".cabecera").sticky({topSpacing:0});
+  });
